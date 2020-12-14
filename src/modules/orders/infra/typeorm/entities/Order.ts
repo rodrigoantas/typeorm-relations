0e53e7029975306e14dfce82c41a94c29c41346f
Rodrigo Antas <rodr.antas@gmail.com>
2020-12-14 18:56:06 -0300

    challenge done

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -11,15 +11,25 @@ import {
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ManyToOne(() => Customer)
+  @JoinColumn({ name: 'customer_id' })
   customer: Customer;
 
+  // fazer one to many e no ordersproducts fazer many to one, pois assim vai virar many to many
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: true, // salva automaticamente os produtos na tabela 'orders_products'
+  })
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,25 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const checkProductExists = await this.productsRepository.findByName(name);
+
+    if (checkProductExists) {
+      throw new AppError('Name already used!');
+    }
+
+    const user = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return user;
   }
 }
 
